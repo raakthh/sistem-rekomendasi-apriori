1cbff2d46caa6ec6d3929a7b549aa51f7837fb7c
raakthh <salmaxyz0@gmail.com>
2026-06-11 21:43:37 +0700

    Fix Render model initialization

--- retail_recommender/wsgi.py
+++ retail_recommender/wsgi.py
@@ -1,3 +1,7 @@
 from retail_recommender.app import create_app
+from retail_recommender.app.model_manager import initialize_models
 
 app = create_app()
+
+with app.app_context():
+    initialize_models(app.config)
